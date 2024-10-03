c7726e480f4f833825c1fe4fcdededec95af3282
ignacioherrer <ignacioherrer@hotmail.com>
2024-10-03 18:16:35 +0100

    Vpc,igw, added to file

--- api_server_db.sh
+++ api_server_db.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-REGION="us-east-1a"
+REGION="us-east-1"
 SUBNET1_PUBLIC-AZ="us-east-1a"
 SUBNET2_PUBLIC-AZ="us-east-1b"
 
 #Create VPC
-aws ec2 create-vpc \
+VPC_ID=$(aws ec2 create-vpc \
     --cidr-block 10.0.0.0/16 \
     --tag-specifications "ResourceType=vpc,Tags=[{Key=Name,Value=vpc_apiproject}]" \
     --region $REGION \
-    --output text 
+    --output text \
+    --query "Vpc.VpcId") 
+echo "VPC $VPC_ID created successfully"
 
 #Create Internet Gateway
+IGW_ID=$(aws ec2 create-internet-gateway \
+    --tag-specifications ResourceType=internet-gateway,Tags=[{Key=Name,Value=igw-apiproject}] \
+    --output text \
+    --query "InternetGateway.InternetGatewayId"
+echo "IGW $IGW_ID created successfully"
+
+#Attach Internet Gateway to VPC
+aws ec2 attach-internet-gateway \
+    --internet-gateway-id $IGW_ID \
+    --vpc-id $VPC_ID
+echo "IGW $IGW_ID successfully attached to VPC $VPC_ID."
