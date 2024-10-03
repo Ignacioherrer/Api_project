a5a48d7608175dc3304589e09b2e30f60d248ff0
ignacioherrer <ignacioherrer@hotmail.com>
2024-10-03 19:40:40 +0100

    fixed typos

--- api_server_db.sh
+++ api_server_db.sh
@@ -15,7 +15,7 @@ echo "VPC $VPC_ID created successfully"
 
 #Create Internet Gateway
 IGW_ID=$(aws ec2 create-internet-gateway \
-    --tag-specifications ResourceType=internet-gateway,Tags=[{Key=Name,Value=igw-apiproject}] \
+    --tag-specifications "ResourceType=internet-gateway,Tags=[{Key=Name,Value=igw-apiproject}]" \
     --output text \
     --query "InternetGateway.InternetGatewayId"
 echo "IGW $IGW_ID created successfully")
@@ -30,7 +30,7 @@ echo "IGW $IGW_ID successfully attached to VPC $VPC_ID."
 SUBNET1_PUBLIC=$(aws ec2 create-subnet \
     --vpc-id $VPC_ID \
     --cidr-block 10.0.0.0/24 \
-    --tag-specifications ResourceType=subnet,Tags=[{Key=Name,Value=public_subnet1_apiproject}] \
+    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=public_subnet1_apiproject}]" \
     --region $REGION \
     --output text \
     --query "Subnet.SubnetId")
@@ -38,12 +38,12 @@ echo "Subnet $SUBNET1_PUBLIC created successfully"
 
 #Enable public subnet to auto-assign public IP
 aws ec2 modify-subnet-attribute --subnet-id $SUBNET1_PUBLIC --map-public-ip-on-launch
-Echo "Public IP auto-assign enabled successfully."
+echo "Public IP auto-assign enabled successfully."
 
 #Create public route table
-RT_PUBLIC=$(aws ec2 create-route-table --vpc-id $VPC_ID --output text -- query "RouteTable.RouteTableId" \
+RT_PUBLIC=$(aws ec2 create-route-table --vpc-id $VPC_ID --output text --query "RouteTable.RouteTableId" \
     --tag-specifications "ResourceType=route-table,Tags=[{Key=Name,Value=public_rt_apiproject}]")
-echo "Rout Table $RT_PUBLIC created successfully."
+echo "Route Table $RT_PUBLIC created successfully."
 
 #Create route to Internet Gateway
 aws ec2 create-route --route-table-id $RT_PUBLIC --destination-cidr-block 0.0.0.0/0 --gateway-id $IGW_ID
@@ -51,4 +51,22 @@ echo "Route to the IGW $IGW_ID created successfully."
 
 #Associate the public subnet 1 with the route table
 aws ec2 associate-route-table --route-table-id $RT_PUBLIC --subnet-id $SUBNET1_PUBLIC
-echo "Subnet $SUBNET_PUBLIC1 associated with route table $RT_PUBLIC"
+echo "Subnet $SUBNET1_PUBLIC associated with route table $RT_PUBLIC"
+
+#Create Public Subnet 2
+SUBNET2_PUBLIC=$(aws ec2 create-subnet \
+    --vpc-id $VPC_ID \
+    --cidr-block 10.0.9.0/24 \
+    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=public_subnet2_apiproject}]" \
+    --region $REGION \
+    --output text \
+    --query "Subnet.SubnetId")
+echo "Subnet $SUBNET2_PUBLIC created successfully"
+
+#Enable public subnet to auto-assign public IP
+aws ec2 modify-subnet-attribute --subnet-id $SUBNET2_PUBLIC --map-public-ip-on-launch
+echo "Public IP auto-assign enabled successfully."
+
+#Associate the public subnet 1 with the route table
+aws ec2 associate-route-table --route-table-id $RT_PUBLIC --subnet-id $SUBNET2_PUBLIC
+echo "Subnet $SUBNET2_PUBLIC associated with route table $RT_PUBLIC"
