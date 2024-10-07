b4c56ff5ce02737eef28b5e192aaef57d2f6e387
ignacioherrer <ignacioherrer@hotmail.com>
2024-10-07 18:20:54 +0100

    added userdata file

--- userdata.sh
+++ userdata.sh
@@ -17,3 +17,20 @@ sudo apt install -y nodejs
 sudo apt install -y mongodb-org
 
 #Download the csv file from the Github repo
+curl -o nhl-stats-2022.csv https://github.com/Ignacioherrer/Api_project/blob/main/nhl-stats-2022.csv
+
+#Set the IP of the MongoDB host
+sudo sed -i 's/127.0.0.1/10.0.10.10,127.0.0.1/g' /etc/mongod.conf
+
+#Start the MongoDB service
+sudo service mongod start
+
+#Service the MongoDB connection details
+MONGO_HOST="10.0.10.10"
+MONGO_PORT="27017"
+MONGO_DB="API_PROJECT"
+COLLECTION_NAME="nhl_stats_2022"
+CSV_FILE="nhl-stats-2022.csv"
+
+#Use mongoimport to import the CSVfile into the specified MongoDB collection
+sudo mongoimport --host $MONGO_HOST --port $MONGO_PORT --db $MongoDB --collection $COLLECTION_NAME --type csv --headerline --file $CSV_FILE
